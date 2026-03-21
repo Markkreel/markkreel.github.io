2d033ca4afcd3c2e248a8ab15493bc6417362a6b
Markkreel <chrystmark2020@mail.ugm.ac.id>
2026-03-22 01:26:11 +0700

    feat: Add ResumePage component to display resume PDF

---
+++ app/resume/page.tsx
@@ -0,0 +1,11 @@
+export default function ResumePage() {
+  return (
+    <div className="h-screen w-full">
+      <iframe
+        src="/Mark_Rumahorbo_Resume.pdf"
+        className="w-full h-full"
+        title="Mark Rumahorbo Resume"
+      />
+    </div>
+  );
+}
